db665e127827d6157da0c7693f44fd52a29c917f
faisal-nadeem-fn <faisal.fn@gmail.com>
2019-04-01 12:57:17 +0500

    added redux

---
+++ react/src/redux/actions.js
@@ -0,0 +1,9 @@
+/**
+ * NOTE :- Example action. 
+ * export const ADD_TODO = 'ADD_TODO';
+ * 
+ * NOTE :- Example action trigger function
+ * export function addTodo(text) {
+ *   return { type: ADD_TODO, text }
+ *  }
+ */
