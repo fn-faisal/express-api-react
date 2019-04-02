6dd9d0fce8a9f0b20562926355c88ebc4a9a01cd
faisal-nadeem-fn <faisal.fn@gmail.com>
2019-04-02 19:48:06 +0500

    react router added

--- react/src/app.js
+++ react/src/app.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {  BrowserRouter as Router, Route, Link  } from 'react-router-dom';
+
+const ROOT_ELEM = document.getElementById("index");
 
 class Index extends React.Component {
     render(){
-        return (<div>Hello React!</div>);
+        return (<div><h2>Hello React!</h2> <Link to='/about'>About</Link></div>);
     }
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+class About extends React.Component {
+    render(){
+        return (<div>About React!</div>);
+    }
+};
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Index} />
+                    <Route path="/about" component={About} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+ReactDOM.render(<App />, ROOT_ELEM );
